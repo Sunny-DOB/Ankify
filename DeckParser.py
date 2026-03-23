cb33f5d15f6ecf223fe7b805bbcecf9a7e7b8873
Sunny-DOB <tom.fiszelson@proton.me>
2026-03-23 09:45:23 +0100

    Created the DeckParser.py file, implemented unzipping of the .apkg file based on a path provided by the user.

---
+++ DeckParser.py
@@ -0,0 +1,15 @@
+from zipfile import ZipFile 
+import os
+
+
+# Deck_path = input("Complete path of the anki deck : ")
+Deck_path = "/home/SunnyDOB/Projects/Ankify/GermanVoc.apkg" # hardcoded path to facilitate development.
+
+try:
+    with ZipFile(Deck_path, 'r') as UnzippedDeck:
+        UnzippedDeck.extract("collection.anki2", path="./")
+    UnzippedDeck.close()
+except FileNotFoundError:
+    print("The file you're trying to access doesn't exist.")
+
+
