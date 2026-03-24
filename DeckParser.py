7c7ef6096e2a5772358ada14f784d771db94ad85
Sunny-DOB <tom.fiszelson@proton.me>
2026-03-25 00:34:51 +0100

    improved csv reading by reducing complexity

--- DeckParser.py
+++ DeckParser.py
@@ -1,8 +1,8 @@
 import csv
 
 with open('German.csv', newline='') as csvfile:
-    fieldnames = ["Word", "Translation"]
-    notes = csv.DictReader(csvfile, fieldnames=fieldnames)
+    #fieldnames = ["Word", "Translation"]
+    notes = csv.reader(csvfile)
     for note in notes:
-        print(note["Word"])
+        print(note[0])
 
