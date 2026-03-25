621f065bb0c97a9ed42a2e667720bffe300ef4cb
Sunny-DOB <tom.fiszelson@proton.me>
2026-03-25 16:11:58 +0100

    modified preprocessing to improve data quality by changing unwated substrings

--- NotesPreprocessing.py
+++ NotesPreprocessing.py
@@ -9,12 +9,13 @@ with open('German.csv', newline='') as csvfile:
     for note in notes:
         UnprocessedNotesSet.add(note[0])
 
-StringsToRemove = ["der", "die", "das", "+A", "+D", "+G", ", en", ", er", ", e", ", ns", ", n", ", ses", ", se", ", es", "-in", ", s", ", i", ", a", ", ei", ", ie", " A ", " D ", "<br>", ";", "(un-)", "(", ")", "+", "-", "&nbsp", "<b>", "qn", "qc", "="] 
+StringsToRemove = ["der ", "die ", "das ", "+A", "+D", "+G", ", en", ", er", ", e", ", ns", ", n", ", ses", ", se", ", es", "-in", ", s", ", i", ", a", ", ei", ", ie", " A ", " D ", "<br>", ";", "(un-)", "(", ")", "+", "-", "&nbsp", "<b>", "qn", "qc", "="] 
 for note in UnprocessedNotesSet:
     for UnwantedSubstring in StringsToRemove:
         note = note.replace(UnwantedSubstring, "")
     note = note.replace(",", " ")
     note = note.replace("/", " ")
+    note = note.replace("\n", " ")
     ProcessedNotesSet.add(note)
 
 with open("ProcessedNotes.txt", "w") as ProcessedNotesFiles:
