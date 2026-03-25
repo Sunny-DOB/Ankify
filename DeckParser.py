822187828cddc8c016ce17bf961fb45b9e172631
Sunny-DOB <tom.fiszelson@proton.me>
2026-03-25 10:13:31 +0100

    added preprocessing by substring removal

--- DeckParser.py
+++ DeckParser.py
@@ -1,13 +1,20 @@
 import csv
 
-NotesSet = set()
+UnprocessedNotesSet = set()
+ProcessedNotesSet = set()
 
 with open('German.csv', newline='') as csvfile:
     #fieldnames = ["Word", "Translation"]
     notes = csv.reader(csvfile)
     for note in notes:
-        NotesSet.add(note[0])
+        UnprocessedNotesSet.add(note[0])
 
-csvfile.close()
-print(NotesSet)
+StringsToRemove = ["der", "die", "das", "+A", "+D", "+G", ", en", ", er", ", e", ", ns", ", n", "(un-)", "/", "(", ")", "+", "-", "&nbsp"]
+for note in UnprocessedNotesSet:
+    for StringToRemove in StringsToRemove:
+        note = note.replace(StringToRemove, "")
+    note = note.replace(",", " ")
+    ProcessedNotesSet.add(note)
+
+print(ProcessedNotesSet)
 
