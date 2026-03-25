285d8400d5b47c90af25328e9dfa31bf9c88c4fc
Sunny-DOB <tom.fiszelson@proton.me>
2026-03-25 09:50:23 +0100

    Adding all the notes to a python set

--- DeckParser.py
+++ DeckParser.py
@@ -1,8 +1,13 @@
 import csv
 
+NotesSet = set()
+
 with open('German.csv', newline='') as csvfile:
     #fieldnames = ["Word", "Translation"]
     notes = csv.reader(csvfile)
     for note in notes:
-        print(note[0])
+        NotesSet.add(note[0])
+
+csvfile.close()
+print(NotesSet)
 
